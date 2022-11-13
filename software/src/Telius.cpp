a178fe3ea6ed73128d4b58d3d18b97bfd2d966b0
Raphael Weber <5662800+raphweb@users.noreply.github.com>
2022-11-13 15:49:56 +0100

    Started display setup.

--- software/src/Telius.cpp
+++ software/src/Telius.cpp
@@ -1,9 +1,21 @@
 #include <Arduino.h>
+#include "DisplayConfig.h"
 
 void setup() {
   Serial.begin(115200);
-
   Serial.println("Ready.");
+
+  display.init();
+  // comment out next line to have no or minimal Adafruit_GFX code
+  display.setTextColor(GxEPD_BLACK);
+  display.firstPage();
+  do
+  {
+    display.fillScreen(GxEPD_WHITE);
+    // comment out next line to have no or minimal Adafruit_GFX code
+    display.print("Hello World!");
+  }
+  while (display.nextPage());
 }
 
 void loop() {
