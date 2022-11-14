4c933505089867c577d5d495fbd7e3656d8f8e01
Raphael Weber <5662800+raphweb@users.noreply.github.com>
2022-11-14 22:17:41 +0100

    Added docu and first table graphic.

--- software/src/Telius.cpp
+++ software/src/Telius.cpp
@@ -4,30 +4,54 @@
 #include <Fonts/FreeSans12pt7b.h>
 #include "DisplayConfig.h"
 
-const char helloWorldStr[] = "Hello colorful world!";
+typedef struct {
+  const char time1[6];
+  const char time2[6];
+  const uint8_t offset;
+} TableRow;
 
-void helloWorld() {
-  display.setTextColor(GxEPD_BLACK);
-  int16_t tbx, tby; uint16_t tbw, tbh;
-  display.getTextBounds(helloWorldStr, 0, 0, &tbx, &tby, &tbw, &tbh);
-  uint16_t x = ((display.width() - tbw) >> 1) - tbx;
-  uint16_t y = ((display.height() - tbh) >> 1) - tby;
-  uint16_t xc = ((display.width() - 140) >> 1);
-  uint16_t yc = y+10;
-  display.setFullWindow();
-  display.firstPage();
-  do {
-    display.fillScreen(GxEPD_WHITE);
-    display.setCursor(x, y);
-    display.print(helloWorldStr);
-    display.fillRect(xc, yc, 20, 20, GxEPD_BLACK);
-    display.fillRect(xc+20, yc, 20, 20, GxEPD_WHITE);
-    display.fillRect(xc+40, yc, 20, 20, GxEPD_BLUE);
-    display.fillRect(xc+60, yc, 20, 20, GxEPD_RED);
-    display.fillRect(xc+80, yc, 20, 20, GxEPD_YELLOW);
-    display.fillRect(xc+100, yc, 20, 20, GxEPD_GREEN);
-    display.fillRect(xc+120, yc, 20, 20, GxEPD_ORANGE);
-  } while (display.nextPage());
+const TableRow schedule[] = {
+  {"08:00", "08:45", 1},
+  {"08:45", "09:30", 3},
+  {"09:30", "10:15", 5},
+  {"10:40", "11:25", 8},
+  {"11:25", "12:10", 10},
+  {"12:10", "12:55", 12}
+};
+
+typedef struct {
+  const char day[11];
+  const uint16_t offset;
+} TableColumn;
+
+const TableColumn days[] = {
+  {"Montag", 84},
+  {"Dienstag", 174},
+  {"Mittwoch", 277},
+  {"Donnerstag", 381},
+  {"Freitag", 514}
+};
+
+const char pauseStr[] = "Pause";
+const uint8_t rowHeight = 30;
+
+void drawTable() {
+  for(const auto& column : days) {
+    display.drawFastVLine(column.offset - 5, 6, display.height() - 12, GxEPD_BLACK);
+    display.setCursor(column.offset, 31);
+    display.print(column.day);
+  }
+  for(const auto& row : schedule) {
+    display.drawFastHLine(0, 11 + rowHeight*row.offset, display.width(), GxEPD_BLACK);
+    display.setCursor(6, 33 + rowHeight*row.offset);
+    display.print(row.time1);
+    display.drawFastHLine(15, 10 + rowHeight + rowHeight*row.offset, 40, GxEPD_BLACK);
+    display.setCursor(6, 33 + rowHeight*(row.offset + 1));
+    display.print(row.time2);
+    display.drawFastHLine(0, 10 +rowHeight*2 + rowHeight*row.offset, display.width(), GxEPD_BLACK);
+  }
+  display.setCursor(6, 33 + rowHeight*7);
+  display.print(pauseStr);
 }
 
 void setup() {
@@ -37,7 +61,13 @@ void setup() {
   display.init(115200, true, 20, false, *(new SPIClass(VSPI)), SPISettings(4000000, MSBFIRST, SPI_MODE0));
   display.setFont(&FreeSans12pt7b);
 
-  helloWorld();
+  display.setTextColor(GxEPD_BLACK);
+  display.setFullWindow();
+  display.firstPage();
+  do {
+    display.fillScreen(GxEPD_WHITE);
+    drawTable();
+  } while (display.nextPage());
 
   display.hibernate();
 }
